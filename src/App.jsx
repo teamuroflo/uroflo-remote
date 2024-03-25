e46d5f5cbc14f21b518b048f3a6149bc09202b8f
achabbi <aac17@rice.edu>
2024-03-25 17:50:23 -0500

    added files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  return (
+    <>
+      <div className="bg-blue-400">asdf</div>
+    </>
+  );
+}
+
+export default App;
